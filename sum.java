ad0657bc1b456a76f3a9950a157f752f0f381093
manojpapneja <70954269+manojpapneja@users.noreply.github.com>
2024-12-09 14:27:11 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 public class sum {
     public static void main(String[] args) {
         int i=4 , j=6 ,k=10;
-        int sum = i+j+k+;
+        int sum = i+j+k+l;
         System.out.println(sum);
     }
 }
